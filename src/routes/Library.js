c9e27e4a4a89e992891d5940a2a32f5b387c34fd
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2022-04-13 23:23:52 +0300

    Made Library use LoadingModal

--- src/routes/Library.js
+++ src/routes/Library.js
@@ -1,32 +1,14 @@
 import BaroofList from '../components/BaroofList';
 import BaroofViewer from '../components/BaroofViewer';
 import {useLibraryContext} from '../hooks/useLibraryContent.js';
-import Modal from '@mui/material/Modal';
-import CircularProgress from '@mui/material/CircularProgress';
-import Box from '@mui/material/Box';
 import {Routes, Route} from 'react-router-dom';
-
-function LoadingModal(){
-	const {loading} = useLibraryContext();
-	return (
-		<Modal open={loading}>
-			<Box sx={{
-				width: "100%",
-				height: "100%",
-				display: "flex",
-				alignItems:"center",
-				justifyContent: "center"
-			}}>
-				<CircularProgress size={100}/>
-			</Box>
-		</Modal>
-	)
-}
+import LoadingModal from '../components/LoadingModal';
 
 export default function Library(){
+	const {loading} = useLibraryContext();
 	return (
 		<>
-			<LoadingModal />
+			<LoadingModal open={loading}/>
 			<Routes>
 				<Route path="/" element={<BaroofList />} />
 				<Route path="/:_id" element={<BaroofViewer />} />
