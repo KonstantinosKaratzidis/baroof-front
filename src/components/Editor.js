1eb7446f37752b89a5344a5dc6554ad1a6a8684c
Konstantinos Karatzidis <kostaskarat@hotmail.com>
2022-04-13 23:00:48 +0300

    Implemented exit from editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,10 +4,17 @@ import {useReducer} from 'react';
 import Box from '@mui/material/Box';
 import EditorToolbar from './EditorToolbar';
 import QuestionEditor from './QuestionEditor';
-import {EditorProvider} from '../hooks/useEditorContext';
+import {EditorProvider, useEditorContext} from '../hooks/useEditorContext';
 import CircularProgress from '@mui/material/CircularProgress';
 import EditorSlides from './EditorSlides';
-import * as _ from 'lodash'
+import * as _ from 'lodash';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import {useNavigate, Navigate} from 'react-router-dom';
 
 const emptyBaroof = {
 	title: "Unititled",
@@ -54,18 +61,20 @@ function useGetBaroof(){
 }
 
 function reducer(state, {action, value}){
-	console.log(action, value);
 	const newState = _.cloneDeep(state);
 	switch(action){
 		case "SAVE":
 			break;
 		case "EXIT":
+			newState.showExitModal = value;
 			break;
 		case "BAROOF_TITLE":
-			newState.baroof.title = value
+			newState.baroof.title = value;
+			newState.hasChanges = true;
 			break;
 		case "QUESTION_TEXT":
 			newState.baroof.questions[state.editIndex].text = value;
+			newState.hasChanges = true;
 			break;
 		case "OPTION":
 			newState.baroof.questions[state.editIndex].options = 
@@ -74,6 +83,7 @@ function reducer(state, {action, value}){
 						return option
 					return {...value};
 				})
+			newState.hasChanges = true;
 			break;
 		case "EDIT_INDEX":
 			newState.editIndex = value;
@@ -81,6 +91,7 @@ function reducer(state, {action, value}){
 		case "SLIDE_ADD":
 			newState.baroof.questions.push(_.cloneDeep(emptyBaroof.questions[0]))
 			newState.editIndex = newState.baroof.questions.length - 1;
+			newState.hasChanges = true;
 			break;
 		case "SLIDE_REMOVE":
 			_.pullAt(newState.baroof.questions, [newState.editIndex])
@@ -88,6 +99,7 @@ function reducer(state, {action, value}){
 				_.clamp(newState.editIndex, 0, newState.baroof.questions.length - 1);
 			if(newState.baroof.questions.length === 0)
 				newState.baroof.questions.push(_.cloneDeep(emptyBaroof.questions[0]))
+			newState.hasChanges = true;
 			break;
 		case "SLIDE_MOVE":
 			const destIndex = newState.editIndex + value;
@@ -98,7 +110,7 @@ function reducer(state, {action, value}){
 				newState.baroof.questions[newState.editIndex];
 			newState.baroof.questions[newState.editIndex] = tmp;
 			newState.editIndex = destIndex;
-
+			newState.hasChanges = true;
 			break;
 		default:
 			console.log("UNKNOWN ACTION !!!")
@@ -107,22 +119,64 @@ function reducer(state, {action, value}){
 	return newState;
 }
 
+function ExitDialog({...props}){
+	const {dispatch} = useEditorContext();
+	const navigate = useNavigate();
+
+	function onExit(){
+		navigate('/user/library');
+	}
+	return (
+		<Dialog {...props} sx={{padding: 2}}>
+			<DialogTitle>
+				<Typography component="p" variant="h4" textAlign="center">
+					Exit Editor ?
+				</Typography>
+			</DialogTitle>
+			<DialogContent>
+				<Typography>
+					If you quit now your changes will be lost
+				</Typography>
+			</DialogContent>
+			<DialogActions>
+				<Button size="large" variant="outlined"
+					color="success"
+					onClick={() =>  {dispatch({action: "EXIT", value: false})}}
+				>
+					Cancel
+				</Button>
+				<Button size="large" variant="contained" color="error"
+					onClick={onExit}
+				>
+					Exit
+				</Button>
+			</DialogActions>
+		</Dialog>
+	)
+}
+
 // workaround for useReducer
 function EditorInner(){
-	const {loading} = useLibraryContext();
 	const baroof = useGetBaroof();
 	const initialState = {
-		loading,
+		loading: false,
 		baroof: _.cloneDeep(baroof),
-		editIndex: 0
+		editIndex: 0,
+		showExitModal: false,
+		hasChanges: false
 	}
 	const [state, dispatch] = useReducer(reducer, initialState);
-	console.log(state.editIndex)
+	console.log("has changes", state.hasChanges);
 
+	if(!state.hasChanges && state.showExitModal)
+		return <Navigate to="/user/library" />
 
 	return (
 		<EditorProvider dispatch={dispatch}>
 			<Box>
+				<ExitDialog open={state.showExitModal}
+					onClose={() => dispatch({action: "EXIT", value: false})}
+				/>
 				<EditorToolbar state={state}/>
 				<EditorSlides state={state}/>
 				<QuestionEditor state={state} />
--- src/components/EditorToolbar.js
+++ src/components/EditorToolbar.js
@@ -12,7 +12,7 @@ export default function EditorToolbar({state}){
 	}
 
 	function onExit(){
-		dispatch({action: "EXIT"})
+		dispatch({action: "EXIT", value: true})
 	}
 
 	function onSave(){
